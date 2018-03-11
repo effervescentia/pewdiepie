ecf7ebeb6b51bd3262695e477fe3f646c1959e8d
Ben Teichman <ben.l.teichman@gmail.com>
2018-03-10 19:21:22 -0500

    next

--- src/elm/Memes.elm
+++ src/elm/Memes.elm
@@ -17,27 +17,36 @@ memes =
     , Meme "loss"
         "10 / 10"
         Images.loss
-        [ "universal", "adaptable", "\"a beautiful meme\"" ]
+        [ "universal"
+        , "adaptable"
+        , "a beautiful meme"
+        ]
         [ width (px 300)
         ]
     , Meme "petting dog"
         "5.5 / 10"
         Images.pettingDog
-        []
+        [ "it has many layers"
+        , "it transcends humans"
+        , "compatible"
+        ]
         [ transform (translateX (px -40))
         , width (px 500)
         ]
     , Meme "monkey haircut"
         "5.2 / 10"
         Images.bikeCuck
-        []
+        [ "it just never stops"
+        , "better than petting dog meme"
+        , "melancholic depression undertone"
+        ]
         [ transform (translateX (px 20))
         , property "filter" "saturate(130%)"
         ]
     , Meme "dahlia"
-        "2.5"
+        "2 and a half"
         Images.bikeCuck
-        []
+        [ "truly mindbending material here" ]
         [ transform (translateX (px 20))
         , property "filter" "saturate(130%)"
         ]
